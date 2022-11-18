f91424576bae3ecd813bd9adf898c4dca6dd08f7
Geremi19 <102634185+Geremi19@users.noreply.github.com>
2022-11-18 12:17:44 -0600

    Add files via upload

---
+++ aaa.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+
+int i,n,aux,min,j;
+printf("Numero del vector: ");
+scanf("%d", &n);
+
+int arraylist[n];
+
+    for(i=0; i<n; i++){
+        printf("Posicion guardar:%d", i);
+        scanf("%d", &arraylist[i]);
+    }
+
+    for(i=0; i<n-1; i++){
+        min=i;
+        printf("/n");
+        for (j= i+1; j<n; j++){
+            if(arraylist[j] < arraylist[min]){
+                min = j;
+            }
+        }
+        printf("/n");
+        if(min != i){
+            aux        = arraylist[min];
+            arraylist[min]     = arraylist[i];
+            arraylist[i]   = aux;
+        }
+    }
+
+	for(i=0; i<n; i++){
+		printf("%d\t",arraylist[i]);
+	}
+
+}
